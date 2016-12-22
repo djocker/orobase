1ac6e7c6f4eed11d7d49907420135fd945221028
SergeyCherepanov <sergey@cherepanov.org.ua>
2016-12-22 05:40:39 +0400

    fxp/composer-asset-plugin:1.2.2

--- setup.sh
+++ setup.sh
@@ -25,33 +25,46 @@ apt-get install -qqy --no-install-recommends php5.6-fpm php5.6-cli php5.6-common
 php5.6-mysql php5.6-curl php5.6-gd php5.6-mcrypt php5.6-sqlite php5.6-xmlrpc php5.6-ldap \
 php5.6-xsl php5.6-intl php5.6-soap php5.6-mbstring php5.6-zip php5.6-bz2 php5.6-redis || exit 1 
 
-# Automatically instal the latest nginx
+# Install nginx
 wget -O - http://nginx.org/keys/nginx_signing.key | sudo apt-key add -
 echo "deb http://nginx.org/packages/mainline/ubuntu/ trusty nginx" >> /etc/apt/sources.list
 echo "deb-src http://nginx.org/packages/mainline/ubuntu/ trusty nginx" >> /etc/apt/sources.list
-
 apt-get -qy update
 apt-get install -qqy --reinstall nginx || exit 1
 
 # Install composer
 (curl -sS https://getcomposer.org/installer | php) || exit 1
-mv composer.phar /usr/local/bin/composer
+mv composer.phar /usr/local/bin/composer.phar
+
+# Create composer home dirs
+mkdir -p -m 0744 /opt/composer/root
+mkdir -p -m 0744 /opt/composer/www-data
+chown www-data:www-data /opt/composer/www-data
+
+
+# Create composer wrapper
+echo '#!/usr/bin/env bash' >> /usr/local/bin/composer
+echo 'COMPOSER_HOME=/opt/composer/$(whoami) /usr/local/bin/composer.phar $@' >> /usr/local/bin/composer
+chmod 0755 /usr/local/bin/composer
+
+# Install required composer-plugins
+sudo -u www-data composer global require fxp/composer-asset-plugin:1.2.2 || exit 1
 
 # Install node.js
 curl -sL https://deb.nodesource.com/setup_4.x | bash -
 apt-get install -qqy nodejs || exit 1
 
 # Install php5 twig extension
-mkdir /tmp/twig && cd /tmp/twig && \
-curl -fSL "https://github.com/twigphp/Twig/archive/v1.24.1.tar.gz" -o twig.tar.gz && \
-tar -xzv --strip-components=1 -f twig.tar.gz && \
-rm twig.tar.gz && \
-cd ext/twig && phpize && \
-./configure && make && make install && \
-cp /tmp/twig/ext/twig/modules/twig.so $(php-config --extension-dir) && \
-cd / && \
-echo 'extension=twig.so' > /etc/php/5.6/fpm/conf.d/20-twig.ini && \
-echo 'extension=twig.so' > /etc/php/5.6/cli/conf.d/20-twig.ini && \
+mkdir /tmp/twig 
+cd /tmp/twig
+curl -fSL "https://github.com/twigphp/Twig/archive/v1.24.1.tar.gz" -o twig.tar.gz || exit 1
+tar -xzv --strip-components=1 -f twig.tar.gz || exit 1
+cd ext/twig && phpize || exit 1
+./configure && make && make install || exit 1
+cp /tmp/twig/ext/twig/modules/twig.so $(php-config --extension-dir) || exit 1
+cd ~
+echo 'extension=twig.so' > /etc/php/5.6/fpm/conf.d/20-twig.ini
+echo 'extension=twig.so' > /etc/php/5.6/cli/conf.d/20-twig.ini
 rm -Rf /tmp/twig
 
 # Install supervisor
