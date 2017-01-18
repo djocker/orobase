8f243f1cf5c34c2c9aa97ac0f28ee7fbcb627506
SergeyCherepanov <sergey@cherepanov.org.ua>
2017-01-18 14:56:30 +0400

    platform 2.x support

--- bin/websocket.sh
+++ bin/websocket.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 /usr/local/bin/waitinstall.sh
-exec /sbin/runuser -s /bin/sh -c "exec /usr/bin/php /var/www/app/console clank:server --env prod" www-data 
+exec /sbin/runuser -s /bin/sh -c "exec /usr/bin/php /var/www/app/console --env=prod clank:server" www-data 
